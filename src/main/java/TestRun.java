3d45b6bbe8017e8d83ee42309881dc50f78ccd24
khoitd1997 <khoidinhtrinh@gmail.com>
2018-06-16 19:59:59 -0700

    added regex for separating title from page

--- src/main/java/TestRun.java
+++ src/main/java/TestRun.java
@@ -8,15 +8,16 @@ import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestRun {
 
     // will take in the page that contain the table of content
     public static void main(String[] args) {
-
             pdfBookmarker bookmarker = new pdfBookmarker(3,5,"/home/kd/PDF_bookmarker/sample_pdf/progit.pdf");
             String pdfText = bookmarker.extractTableContent();
-            System.out.print(pdfText);
+//            System.out.print(pdfText);
     }
 }
     class pdfBookmarker {
@@ -37,6 +38,9 @@ public class TestRun {
 
         public String extractTableContent() {
             String pdfText;
+            String delimiter = ". ";
+
+
             try {
                 PDFTextStripper pdfStripper = new PDFTextStripper();
                 pdfStripper.setStartPage(startPage);
@@ -46,6 +50,33 @@ public class TestRun {
             } catch (IOException ioException) {
                 pdfText = new String("");
                 ioException.printStackTrace();
+            }
+            String[] pdfTextLines = pdfText.split("\n");
+
+            int i = 0;
+            for(String line:pdfTextLines) {
+                //{2,}[\\t\\n\\x0B\\f\\r]*[0-9]+
+                String[] temp = line.split("[\\t\\n\\x0B\\f\\r]*" +  "(" + "\\" + delimiter +  ")+" + "[\\t\\n\\x0B\\f\\r]*");
+
+                for(String tempString:temp){
+                    System.out.print(tempString + "             ");
+                }
+                System.out.println("");
+
+//                if(temp.length>1) {
+//                    System.out.println("Title:" + temp[0]);
+//
+//                    temp[1] = temp[1].replace("[" + delimiter + "]* \\1 \\s", "");
+//                    System.out.println("Location:" + temp[1]);
+//                }
+
+
+                ++ i;
+                if (i > 6) {
+                    while (true) {
+
+                    }
+                }
             }
             return pdfText;
         }
